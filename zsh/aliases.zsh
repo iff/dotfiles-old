82648519d414da245e2fcb9402689a46333196fe
iff <iff@yvesineichen.com>
2012-12-09 08:50:59 +0100

    adding part of holmans zsh configuration

---
+++ zsh/aliases.zsh
@@ -0,0 +1,14 @@
+alias reload!='. ~/.zshrc'
+
+alias 'l=ls -alF'
+alias 'la=ls -la'
+alias 'll=ls -l'
+alias 'ls-l=ls -l'
+alias 'md=mkdir -p'
+alias 'open=xdg-open'
+alias 'mutt=TERM=rxvt-256-color /home/iff/local/src/mutt-1.5.21/mutt'
+
+# ssh
+alias 'gof=ssh -YCAt ineichen@llcx.psi.ch ssh -YCA ineichen@felsim00.psi.ch'
+alias 'gow=ssh -YCA iff@yvesineichen.com'
+alias 'goo=ssh -YC ineichen@obiwan.ethz.ch'
---
+++ zsh/prompt.zsh
@@ -0,0 +1,71 @@
+autoload colors && colors
+# cheers, @ehrenmurdick
+# http://github.com/ehrenmurdick/config/blob/master/zsh/prompt.zsh
+
+git_branch() {
+  echo $(/usr/bin/git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})
+}
+
+git_dirty() {
+  st=$(/usr/bin/git status 2>/dev/null | tail -n 1)
+  if [[ $st == "" ]]
+  then
+    echo ""
+  else
+    if [[ $st == "nothing to commit (working directory clean)" ]]
+    then
+      echo "on %{$fg_bold[green]%}$(git_prompt_info)%{$reset_color%}"
+    else
+      echo "on %{$fg_bold[red]%}$(git_prompt_info)%{$reset_color%}"
+    fi
+  fi
+}
+
+git_prompt_info () {
+ ref=$(/usr/bin/git symbolic-ref HEAD 2>/dev/null) || return
+# echo "(%{\e[0;33m%}${ref#refs/heads/}%{\e[0m%})"
+ echo "${ref#refs/heads/}"
+}
+
+unpushed () {
+  /usr/bin/git cherry -v @{upstream} 2>/dev/null
+}
+
+need_push () {
+  if [[ $(unpushed) == "" ]]
+  then
+    echo " "
+  else
+    echo " with %{$fg_bold[magenta]%}unpushed%{$reset_color%} "
+  fi
+}
+
+sec_up() {
+  if $(which /usr/lib/update-notifier/apt-check &> /dev/null)
+  then
+    #num=$(/usr/lib/update-notifier/apt-check 2>&1 | cut -d ';' -f 2)
+    #if [ $num != 0 ]
+    #then
+    #  echo "$num"
+    #else
+    #  echo ""
+    #fi
+    echo ""
+  else
+    echo ""
+  fi
+}
+
+directory_name(){
+  echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
+}
+
+export PROMPT=$'\n%{$fg_bold[yellow]%}%n%{$reset_color%} in $(directory_name) $(git_dirty)$(need_push)\n› '
+set_prompt () {
+  export RPROMPT="%{$fg_bold[red]%}$(sec_up) %{$fg_bold[cyan]%}%T%{$reset_color%}"
+}
+
+precmd() {
+  title "zsh" "%m" "%55<...<%~"
+  set_prompt
+}
