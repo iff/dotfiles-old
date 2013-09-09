1fd74d13d214188c0e4a610a70dbe394cbdbe8e6
Yves Ineichen <iff@yvesineichen.com>
2013-09-09 12:26:28 +0200

    using openmpi with different wrapper compilers

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -11,3 +11,7 @@ alias 'mutt=TERM=rxvt-256-color /home/iff/local/src/mutt-1.5.21/mutt'
 # ssh
 alias 'gof=ssh -YCAt ineichen@llcx.psi.ch ssh -YCA ineichen@felsim00.psi.ch'
 alias 'gow=ssh -YCA iff@yvesineichen.com'
+
+# mpicxx wrappers
+alias 'mpicxx-clang=OMPI_CXX=g++ mpicxx'
+alias 'mpicxx-gcc=OMPI_CXX=g++ mpicxx'
